3d798233c7a853f1420ac6b46f2480716f6a8f95
jhasselbring <janmykhail.hasselbring@outlook.com>
2025-01-20 11:52:40 -0600

    init

---
+++ index.js
@@ -0,0 +1,251 @@
+export default {
+    "avi":{
+        "type": "video"
+    },
+    "mp4":{
+        "type": "video"
+    },
+    "mov":{
+        "type": "video"
+    },
+    "wmv":{
+        "type": "video"
+    },
+    "mpg":{
+        "type": "video"
+    },
+    "flv":{
+        "type": "video"
+    },
+    "rm":{
+        "type": "video"
+    },
+    "rmvb":{
+        "type": "video"
+    },
+    "mkv":{
+        "type": "video"
+    },
+    "divx":{
+        "type": "video"
+    },
+    "asf":{
+        "type": "video"
+    },
+    "png":{
+        "type": "image"
+    },
+    "jpg":{
+        "type": "image"
+    },
+    "mts":{
+        "type": "video"
+    },
+    "jpeg":{
+        "type": "image"
+    },
+    "mpeg":{
+        "type": "video"
+    },
+    "7z":{
+        "type": "archive"
+    },
+    "m4v":{
+        "type": "video"
+    },
+    "zip":{
+        "type": "archive"
+    },
+    "ogg":{
+        "type": "audio"
+    },
+    "mht":{
+        "type": "web"
+    },
+    "txt":{
+        "type": "text"
+    },
+    "3gp":{
+        "type": "video"
+    },
+    "ogm":{
+        "type": "video"
+    },
+    "ogv":{
+        "type": "video"
+    },
+    "torrent":{
+        "type": "torrent"
+    },
+    "csv":{
+        "type": "data"
+    },
+    "rar":{
+        "type": "archive"
+    },
+    "xvid":{
+        "type": "video"
+    },
+    "7zip":{
+        "type": "archive"
+    },
+    "webm":{
+        "type": "video"
+    },
+    "mpeg4":{
+        "type": "video"
+    },
+    "xz":{
+        "type": "archive"
+    },
+    "tar":{
+        "type": "archive"
+    },
+    "pdf":{
+        "type": "document"
+    },
+    "gif":{
+        "type": "image"
+    },
+    "url":{
+        "type": "web"
+    },
+    "jfif":{
+        "type": "image"
+    },
+    "xcf":{
+        "type": "image"
+    },
+    "dctmp":{
+        "type": "image"
+    },
+    "mega":{
+        "type": "web"
+    },
+    "3g2":{
+        "type": "video"
+    },
+    "vob":{
+        "type": "video"
+    },
+    "bmp":{
+        "type": "image"
+    },
+    "ts":{
+        "type": "video"
+    },
+    "crc":{
+        "type": "video"
+    },
+    "webp":{
+        "type": "image"
+    },
+    "3gpp":{
+        "type": "video"
+    },
+    "mpe":{
+        "type": "video"
+    },
+    "jpe":{
+        "type": "image"
+    },
+    "diz":{
+        "type": "text"
+    },
+    "tif":{
+        "type": "image"
+    },
+    "css":{
+        "type": "web"
+    },
+    "html":{
+        "type": "web"
+    },
+    "js":{
+        "type": "web"
+    },
+    "eot_":{
+        "type": "font"
+    },
+    "1":{
+        "type": "image"
+    },
+    "flac":{
+        "type": "audio"
+    },
+    "ico":{
+        "type": "image"
+    },
+    "db":{
+        "type": "database"
+    },
+    "thm":{
+        "type": "image"
+    },
+    "ini":{
+        "type": "text"
+    },
+    "ion":{
+        "type": "text"
+    },
+    "sha1":{
+        "type": "text"
+    },
+    "dat":{
+        "type": "data"
+    },
+    "sha256":{
+        "type": "text"
+    },
+    "m2ts":{
+        "type": "video"
+    },
+    "nfo":{
+        "type": "text"
+    },
+    "sfv":{
+        "type": "text"
+    },
+    "ds_store":{
+        "type": "system"
+    },
+    "mp3":{
+        "type": "audio"
+    },
+    "0":{
+        "type": "system"
+    },
+    "md5":{
+        "type": "text"
+    },
+    "htm":{
+        "type": "web"
+    },
+    "psd":{
+        "type": "image"
+    },
+    "ttf":{
+        "type": "font"
+    },
+    "eot":{
+        "type": "font"
+    },
+    "svg":{
+        "type": "image"
+    },
+    "woff":{
+        "type": "font"
+    },
+    "tbz":{
+        "type": "archive"
+    },
+    "loaded":{
+        "type": "system"
+    },
+    "part":{
+        "type": "system"
+    },
+    "package":{
+        "type": "system"
+    }
+}
