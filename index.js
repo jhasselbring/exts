0a1e90be084a1abdf09fa5d11674d2b391add0eb
JHasselbring <janmykhail.hasselbring@outlook.com>
2025-03-30 14:12:16 -0500

    update

--- index.js
+++ index.js
@@ -1,251 +1,417 @@
 export default {
-    "avi":{
-        "type": "video"
-    },
-    "mp4":{
-        "type": "video"
-    },
-    "mov":{
-        "type": "video"
-    },
-    "wmv":{
-        "type": "video"
-    },
-    "mpg":{
-        "type": "video"
-    },
-    "flv":{
-        "type": "video"
-    },
-    "rm":{
-        "type": "video"
-    },
-    "rmvb":{
-        "type": "video"
-    },
-    "mkv":{
-        "type": "video"
-    },
-    "divx":{
-        "type": "video"
-    },
-    "asf":{
-        "type": "video"
-    },
-    "png":{
-        "type": "image"
-    },
-    "jpg":{
-        "type": "image"
-    },
-    "mts":{
-        "type": "video"
-    },
-    "jpeg":{
-        "type": "image"
-    },
-    "mpeg":{
-        "type": "video"
-    },
-    "7z":{
-        "type": "archive"
-    },
-    "m4v":{
-        "type": "video"
-    },
-    "zip":{
-        "type": "archive"
-    },
-    "ogg":{
-        "type": "audio"
-    },
-    "mht":{
-        "type": "web"
-    },
-    "txt":{
-        "type": "text"
-    },
-    "3gp":{
-        "type": "video"
-    },
-    "ogm":{
-        "type": "video"
-    },
-    "ogv":{
-        "type": "video"
-    },
-    "torrent":{
-        "type": "torrent"
-    },
-    "csv":{
-        "type": "data"
-    },
-    "rar":{
-        "type": "archive"
-    },
-    "xvid":{
-        "type": "video"
-    },
-    "7zip":{
-        "type": "archive"
-    },
-    "webm":{
-        "type": "video"
-    },
-    "mpeg4":{
-        "type": "video"
-    },
-    "xz":{
-        "type": "archive"
-    },
-    "tar":{
-        "type": "archive"
-    },
-    "pdf":{
-        "type": "document"
-    },
-    "gif":{
-        "type": "image"
-    },
-    "url":{
-        "type": "web"
-    },
-    "jfif":{
-        "type": "image"
-    },
-    "xcf":{
-        "type": "image"
-    },
-    "dctmp":{
-        "type": "image"
-    },
-    "mega":{
-        "type": "web"
-    },
-    "3g2":{
-        "type": "video"
-    },
-    "vob":{
-        "type": "video"
-    },
-    "bmp":{
-        "type": "image"
-    },
-    "ts":{
-        "type": "video"
-    },
-    "crc":{
-        "type": "video"
-    },
-    "webp":{
-        "type": "image"
-    },
-    "3gpp":{
-        "type": "video"
-    },
-    "mpe":{
-        "type": "video"
-    },
-    "jpe":{
-        "type": "image"
-    },
-    "diz":{
-        "type": "text"
-    },
-    "tif":{
-        "type": "image"
-    },
-    "css":{
-        "type": "web"
-    },
-    "html":{
-        "type": "web"
-    },
-    "js":{
-        "type": "web"
-    },
-    "eot_":{
-        "type": "font"
+    "0":{
+        "type": "unknown",
+        "description": "Generic numbered file or system file",
+        "mime-type": "application/octet-stream"
     },
     "1":{
-        "type": "image"
+        "type": "unknown",
+        "description": "Generic numbered file or system file",
+        "mime-type": "application/octet-stream"
     },
-    "flac":{
-        "type": "audio"
+    "3g2":{
+        "type": "video",
+        "description": "3GPP2 multimedia file format used by mobile phones",
+        "mime-type": "video/3gpp2"
     },
-    "ico":{
-        "type": "image"
+    "3gp":{
+        "type": "video",
+        "description": "3GPP multimedia file format used by mobile phones",
+        "mime-type": "video/3gpp"
     },
-    "db":{
-        "type": "database"
+    "3gpp":{
+        "type": "video",
+        "description": "3GPP multimedia file format used by mobile phones",
+        "mime-type": "video/3gpp"
     },
-    "thm":{
-        "type": "image"
+    "7z":{
+        "type": "archive",
+        "description": "7-Zip compressed file format with high compression ratio",
+        "mime-type": "application/x-7z-compressed"
     },
-    "ini":{
-        "type": "text"
+    "7zip":{
+        "type": "archive",
+        "description": "7-Zip compressed file format with high compression ratio",
+        "mime-type": "application/x-7z-compressed"
     },
-    "ion":{
-        "type": "text"
+    "asf":{
+        "type": "video",
+        "description": "Advanced Systems Format container for streaming media",
+        "mime-type": "video/x-ms-asf"
     },
-    "sha1":{
-        "type": "text"
+    "avi":{
+        "type": "video",
+        "description": "Audio Video Interleave multimedia container format",
+        "mime-type": "video/x-msvideo"
+    },
+    "bmp":{
+        "type": "image",
+        "description": "Bitmap image file format",
+        "mime-type": "image/bmp"
+    },
+    "crc":{
+        "type": "application",
+        "description": "Cyclic Redundancy Check file for verifying file integrity",
+        "mime-type": "application/octet-stream"
+    },
+    "css":{
+        "type": "web",
+        "description": "Cascading Style Sheets for styling web pages",
+        "mime-type": "text/css"
+    },
+    "csv":{
+        "type": "data",
+        "description": "Comma-Separated Values file for tabular data",
+        "mime-type": "text/csv"
     },
     "dat":{
-        "type": "data"
+        "type": "data",
+        "description": "Generic data file containing program information",
+        "mime-type": "application/octet-stream"
     },
-    "sha256":{
-        "type": "text"
+    "db":{
+        "type": "database",
+        "description": "Database file containing structured data",
+        "mime-type": "application/vnd.sqlite3"
     },
-    "m2ts":{
-        "type": "video"
+    "dctmp":{
+        "type": "application",
+        "description": "Temporary image file",
+        "mime-type": "application/octet-stream"
     },
-    "nfo":{
-        "type": "text"
+    "divx":{
+        "type": "video",
+        "description": "DivX video file format with high compression",
+        "mime-type": "video/divx"
     },
-    "sfv":{
-        "type": "text"
+    "diz":{
+        "type": "text",
+        "description": "Description In Zip file containing information about archive contents",
+        "mime-type": "text/plain"
     },
     "ds_store":{
-        "type": "system"
-    },
-    "mp3":{
-        "type": "audio"
-    },
-    "0":{
-        "type": "system"
-    },
-    "md5":{
-        "type": "text"
-    },
-    "htm":{
-        "type": "web"
-    },
-    "psd":{
-        "type": "image"
-    },
-    "ttf":{
-        "type": "font"
+        "type": "system",
+        "description": "macOS Desktop Services Store file containing folder metadata",
+        "mime-type": "application/octet-stream"
     },
     "eot":{
-        "type": "font"
+        "type": "font",
+        "description": "Embedded OpenType font for web pages",
+        "mime-type": "application/vnd.ms-fontobject"
     },
-    "svg":{
-        "type": "image"
+    "eot_":{
+        "type": "font",
+        "description": "Embedded OpenType font file variant",
+        "mime-type": "application/vnd.ms-fontobject"
     },
-    "woff":{
-        "type": "font"
+    "flac":{
+        "type": "audio",
+        "description": "Free Lossless Audio Codec file with high audio quality",
+        "mime-type": "audio/flac"
     },
-    "tbz":{
-        "type": "archive"
+    "flv":{
+        "type": "video",
+        "description": "Flash Video file format used for streaming",
+        "mime-type": "video/x-flv"
+    },
+    "gif":{
+        "type": "image",
+        "description": "Graphics Interchange Format for animated images",
+        "mime-type": "image/gif"
+    },
+    "htm":{
+        "type": "web",
+        "description": "HyperText Markup Language file for web pages",
+        "mime-type": "text/html"
+    },
+    "html":{
+        "type": "web",
+        "description": "HyperText Markup Language file for web pages",
+        "mime-type": "text/html"
+    },
+    "ico":{
+        "type": "image",
+        "description": "Icon file format used for website favicons",
+        "mime-type": "image/x-icon"
+    },
+    "ini":{
+        "type": "text",
+        "description": "Initialization file containing configuration settings",
+        "mime-type": "text/plain"
+    },
+    "ion":{
+        "type": "text",
+        "description": "Ion data format file",
+        "mime-type": "text/plain"
+    },
+    "jfif":{
+        "type": "image",
+        "description": "JPEG File Interchange Format image",
+        "mime-type": "image/jpeg"
+    },
+    "jpe":{
+        "type": "image",
+        "description": "JPEG image file alternative extension",
+        "mime-type": "image/jpeg"
+    },
+    "jpeg":{
+        "type": "image",
+        "description": "Joint Photographic Experts Group compressed image",
+        "mime-type": "image/jpeg"
+    },
+    "jpg":{
+        "type": "image",
+        "description": "Joint Photographic Experts Group compressed image",
+        "mime-type": "image/jpeg"
+    },
+    "js":{
+        "type": "web",
+        "description": "JavaScript code file for web functionality",
+        "mime-type": "application/javascript"
     },
     "loaded":{
-        "type": "system"
+        "type": "system",
+        "description": "System file indicating loaded resources",
+        "mime-type": "application/octet-stream"
     },
-    "part":{
-        "type": "system"
+    "m2ts":{
+        "type": "video",
+        "description": "MPEG-2 Transport Stream for Blu-ray disc video",
+        "mime-type": "video/mp2t"
+    },
+    "m4v":{
+        "type": "video",
+        "description": "MPEG-4 video file format commonly used by Apple",
+        "mime-type": "video/x-m4v"
+    },
+    "md5":{
+        "type": "text",
+        "description": "MD5 checksum file for verifying file integrity",
+        "mime-type": "text/plain"
+    },
+    "mega":{
+        "type": "text",
+        "description": "MEGA cloud storage link file",
+        "mime-type": "text/plain"
+    },
+    "mht":{
+        "type": "web",
+        "description": "MIME HTML file archiving web pages with resources",
+        "mime-type": "message/rfc822"
+    },
+    "mkv":{
+        "type": "video",
+        "description": "Matroska Video file container format",
+        "mime-type": "video/x-matroska"
+    },
+    "mov":{
+        "type": "video",
+        "description": "Apple QuickTime movie file format",
+        "mime-type": "video/quicktime"
+    },
+    "mp3":{
+        "type": "audio",
+        "description": "MPEG Audio Layer III compressed audio file",
+        "mime-type": "audio/mpeg"
+    },
+    "mp4":{
+        "type": "video",
+        "description": "MPEG-4 video file format",
+        "mime-type": "video/mp4"
+    },
+    "mpe":{
+        "type": "video",
+        "description": "MPEG video file format",
+        "mime-type": "video/mpeg"
+    },
+    "mpeg":{
+        "type": "video",
+        "description": "Moving Picture Experts Group video file",
+        "mime-type": "video/mpeg"
+    },
+    "mpeg4":{
+        "type": "video",
+        "description": "MPEG-4 video file format",
+        "mime-type": "video/mp4"
+    },
+    "mpg":{
+        "type": "video",
+        "description": "Moving Picture Experts Group video file",
+        "mime-type": "video/mpeg"
+    },
+    "mts":{
+        "type": "video",
+        "description": "AVCHD video file format used by HD camcorders",
+        "mime-type": "video/mp2t"
+    },
+    "nfo":{
+        "type": "text",
+        "description": "Information file containing details about media files",
+        "mime-type": "text/plain"
+    },
+    "ogg":{
+        "type": "audio",
+        "description": "Ogg Vorbis audio file format",
+        "mime-type": "audio/ogg"
+    },
+    "ogm":{
+        "type": "video",
+        "description": "Ogg Media file format for audio and video",
+        "mime-type": "video/ogg"
+    },
+    "ogv":{
+        "type": "video",
+        "description": "Ogg Video file format",
+        "mime-type": "video/ogg"
     },
     "package":{
-        "type": "system"
+        "type": "system",
+        "description": "Software package or installation file",
+        "mime-type": "application/octet-stream"
+    },
+    "part":{
+        "type": "system",
+        "description": "Partially downloaded file",
+        "mime-type": "application/octet-stream"
+    },
+    "pdf":{
+        "type": "document",
+        "description": "Portable Document Format for preserving document formatting",
+        "mime-type": "application/pdf"
+    },
+    "png":{
+        "type": "image",
+        "description": "Portable Network Graphics image with lossless compression",
+        "mime-type": "image/png"
+    },
+    "psd":{
+        "type": "application",
+        "description": "Adobe Photoshop Document with layers and editing data",
+        "mime-type": "application/x-photoshop"
+    },
+    "rar":{
+        "type": "archive",
+        "description": "Roshal Archive compressed file format",
+        "mime-type": "application/vnd.rar"
+    },
+    "rm":{
+        "type": "video",
+        "description": "RealMedia streaming media format",
+        "mime-type": "application/vnd.rn-realmedia"
+    },
+    "rmvb":{
+        "type": "video",
+        "description": "RealMedia Variable Bitrate file format",
+        "mime-type": "application/vnd.rn-realmedia-vbr"
+    },
+    "sfv":{
+        "type": "text",
+        "description": "Simple File Verification for checking file integrity",
+        "mime-type": "text/plain"
+    },
+    "sha1":{
+        "type": "text",
+        "description": "SHA-1 checksum file for verifying file integrity",
+        "mime-type": "text/plain"
+    },
+    "sha256":{
+        "type": "text",
+        "description": "SHA-256 checksum file for verifying file integrity",
+        "mime-type": "text/plain"
+    },
+    "svg":{
+        "type": "image",
+        "description": "Scalable Vector Graphics image file",
+        "mime-type": "image/svg+xml"
+    },
+    "tar":{
+        "type": "archive",
+        "description": "Tape Archive file for collecting multiple files",
+        "mime-type": "application/x-tar"
+    },
+    "tbz":{
+        "type": "archive",
+        "description": "Bzip2 compressed tar archive",
+        "mime-type": "application/x-bzip-compressed-tar"
+    },
+    "thm":{
+        "type": "image",
+        "description": "Thumbnail image file",
+        "mime-type": "image/jpeg"
+    },
+    "tif":{
+        "type": "image",
+        "description": "Tagged Image File Format for high-quality images",
+        "mime-type": "image/tiff"
+    },
+    "torrent":{
+        "type": "application",
+        "description": "BitTorrent file for peer-to-peer file sharing",
+        "mime-type": "application/x-bittorrent"
+    },
+    "ts":{
+        "type": "video",
+        "description": "MPEG Transport Stream video format for broadcasting",
+        "mime-type": "video/mp2t"
+    },
+    "ttf":{
+        "type": "font",
+        "description": "TrueType Font file format",
+        "mime-type": "font/ttf"
+    },
+    "txt":{
+        "type": "text",
+        "description": "Plain text file without formatting",
+        "mime-type": "text/plain"
+    },
+    "url":{
+        "type": "web",
+        "description": "Internet shortcut file containing a web address",
+        "mime-type": "application/internet-shortcut"
+    },
+    "vob":{
+        "type": "video",
+        "description": "DVD Video Object file format",
+        "mime-type": "video/x-ms-vob"
+    },
+    "webm":{
+        "type": "video",
+        "description": "WebM open media file format for web video",
+        "mime-type": "video/webm"
+    },
+    "webp":{
+        "type": "image",
+        "description": "WebP image format with compression for the web",
+        "mime-type": "image/webp"
+    },
+    "wmv":{
+        "type": "video",
+        "description": "Windows Media Video file format",
+        "mime-type": "video/x-ms-wmv"
+    },
+    "woff":{
+        "type": "font",
+        "description": "Web Open Font Format for use on websites",
+        "mime-type": "font/woff"
+    },
+    "xcf":{
+        "type": "image",
+        "description": "GIMP image file format with layers",
+        "mime-type": "image/x-xcf"
+    },
+    "xvid":{
+        "type": "video",
+        "description": "Xvid video codec file",
+        "mime-type": "video/x-xvid"
+    },
+    "xz":{
+        "type": "archive",
+        "description": "XZ compressed archive with high compression ratio",
+        "mime-type": "application/x-xz"
+    },
+    "zip":{
+        "type": "archive",
+        "description": "ZIP compressed archive format",
+        "mime-type": "application/zip"
     }
 }
